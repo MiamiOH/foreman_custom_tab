2e6639d0ca3359368b144bf5ea4588cf5c91cc1d
Nipendar Tyagi <ntyagi@appriss.com>
2017-11-24 12:13:02 -0500

    fixed issue with Custom Tab data display It now displays host data as per configuration fields

--- app/helpers/concerns/foreman_custom_tab/hosts_helper_extensions.rb
+++ app/helpers/concerns/foreman_custom_tab/hosts_helper_extensions.rb
@@ -2,14 +2,13 @@ module ForemanCustomTab
   module HostsHelperExtensions
     extend ActiveSupport::Concern
     
-    def custom_tab_fields(host)
+    def custom_tab_fields(host)      
       fields = []
       rows = SETTINGS[:custom_tab][:fields] || []
       rows.each do |name, value|
         result = nil
-        value.split('.').each_with_index do |method, index|
-          result = index.eql?(0) ? host.try(method) : result.try(method)
-        end
+        name = name.parameterize.underscore
+        result = host.try(name) 
         fields += [[_(name.to_s), result]] if result.present?
       end
       fields
