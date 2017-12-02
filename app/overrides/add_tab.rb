c37a5a1870875577de3ab9de4dcaf78529d6aea8
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-01 22:35:03 -0500

    fixed routes, Rakefile and views rubocop errors

--- app/overrides/add_tab.rb
+++ app/overrides/add_tab.rb
@@ -1,8 +1,8 @@
 if SETTINGS[:custom_tab]
   Deface::Override.new(
-                     :virtual_path => "hosts/show",
-                     :name => "add_tab_link",
-                     :insert_bottom => "ul#myTab",
-                     :text => "  <li><a href=\"#custom_tab\" data-toggle=\"tab\"><%= _(custom_tab_title) %></a></li>"
+                       :virtual_path => 'hosts/show',
+                       :name => 'add_tab_link',
+                       :insert_bottom => 'ul#myTab',
+                       :text => "<li><a href=\"#custom_tab\" data-toggle=\"tab\"><%= _(custom_tab_title) %></a></li>"
                      )
 end
