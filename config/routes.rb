f919ff762f30a02c31c21bab3dd62be46f459158
Nipendar Tyagi <ntyagi@appriss.com>
2017-12-01 21:28:56 -0500

    rstrip trailing spaces and other fixes, down to 12 issues mostly in overrides

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  #scope :foreman_custom_tab, :path => '/custom_tab' do
-    constraints(:id => /[^\/]+/) do
-      resources :hosts do
-        member do
-          get 'custom_tab'
-        end
+  constraints(:id => /[^\/]+/) do
+    resources :hosts do
+      member do
+        get 'custom_tab'
       end
     end
-  #end
+  end
 end
