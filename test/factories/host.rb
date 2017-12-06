2e7dbb9d440422c83cf1ecd54ab2bc79ff0b1205
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-06 00:48:15 -0500

    fixed Factory usage in plugin tests Todo: fix why custom_tab settings are not loaded in test env

---
+++ test/factories/host.rb
@@ -0,0 +1,15 @@
+factory :os do
+    name 'CentOS'
+    major '7'
+    type 'Redhat'
+end
+
+factory :host do
+    name 'foreman_custom_tab'
+    mac '00:00:00:00:00:00'
+    certname 'foreman_custom_tab'
+    ip '127.0.0.1'
+    certname 'host.test.com'
+    arch FactoryBot.create(:architecture)
+    operatingsystem os
+end
--- test/functional/concerns/hosts_controller_extensions_test.rb
+++ test/functional/concerns/hosts_controller_extensions_test.rb
@@ -1,20 +1,33 @@
 require 'test_plugin_helper'
 
 class HostsControllerTest < ActionController::TestCase  
-  
+  let(:os){FactoryBot.create(:operatingsystem, name: 'CentOS', major: '7', type: 'Redhat')}
+  let(:arch){FactoryBot.create(:architecture)}
+  let(:host){FactoryBot.create(:host, id: 'foreman.example.com', mac: '00:00:00:00:00:00', ip: '127.0.0.1', operatingsystem: os, arch: arch)}
+
   test 'GET hosts/:id displays tabs' do
-    host = mock_host
     get :show, { :id => host.id }, set_session_user
     assert_includes response.headers['Content-Type'], 'text/html'
+    assert_equal response.status, 200
     assert_includes response.body, "ul id=\"myTab\""
   end
 
-  test "GET host tabs include 'metrics', 'nics'" do
-    host = mock_host
+  test "host overview displays tabs 'Properties', 'Metrics', 'NICs'" do
+    get :show, { :id => host.id }, set_session_user
+    assert_includes response.headers['Content-Type'], 'text/html'
+    assert_equal response.status, 200
+    assert_includes response.body, "<ul id=\"myTab\""
+    assert_includes response.body, "<li class=\"active\"><a href=\"#properties\" data-toggle=\"tab\">Properties</a></li>"
+    assert_includes response.body, "<li><a href=\"#metrics\" data-toggle=\"tab\">Metrics</a></li>"
+    assert_includes response.body, "<li><a href=\"#nics\" data-toggle=\"tab\">NICs</a></li>"
+  end
+
+  test "host overview displays Custom Tab" do
+    skip("need to fix settings in test ENV")
     get :show, { :id => host.id }, set_session_user
     assert_includes response.headers['Content-Type'], 'text/html'
     assert_includes response.body, "<ul id=\"myTab\""
-    assert_includes response.body, "<li><a href=\"#metrics\" data-toggle=\"tab\">Metrics</a></li>"
-    assert_includes response.body, "<li><a href=\"#nics\" data-toggle=\"tab\">NICs</a></li>"
+    assert_equal response.status, 200
+    assert_includes response.body, "Custom Tab"
   end
 end
