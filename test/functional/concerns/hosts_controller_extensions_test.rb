16bf9bb54a2e5008e61356f42dc165532890ab75
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-08 00:42:00 -0500

    test to verify Custom Tab is displayed passes make sure to add custom tab settings to settings.yaml.test

--- test/functional/concerns/hosts_controller_extensions_test.rb
+++ test/functional/concerns/hosts_controller_extensions_test.rb
@@ -3,8 +3,8 @@ require 'test_plugin_helper'
 class HostsControllerTest < ActionController::TestCase  
   let(:os){FactoryBot.create(:operatingsystem, name: 'CentOS', major: '7', type: 'Redhat')}
   let(:arch){FactoryBot.create(:architecture)}
-  let(:host){FactoryBot.create(:host, id: 'foreman.example.com', mac: '00:00:00:00:00:00', ip: '127.0.0.1', operatingsystem: os, arch: arch)}
-
+  let(:host){FactoryBot.create(:host, id: 'foreman.example.com', mac: '00:00:00:00:00:00', ip: '127.0.0.1', operatingsystem: os, arch: arch)}         
+  let(:custom_tab_title){SETTINGS[:custom_tab][:title]}
   test 'GET hosts/:id displays tabs' do
     get :show, { :id => host.id }, set_session_user
     assert_includes response.headers['Content-Type'], 'text/html'
@@ -22,12 +22,11 @@ class HostsControllerTest < ActionController::TestCase
     assert_includes response.body, "<li><a href=\"#nics\" data-toggle=\"tab\">NICs</a></li>"
   end
 
-  test "host overview displays Custom Tab" do
-    skip("need to fix settings in test ENV")
+  test "host overview displays the Custom Tab" do
     get :show, { :id => host.id }, set_session_user
     assert_includes response.headers['Content-Type'], 'text/html'
     assert_includes response.body, "<ul id=\"myTab\""
     assert_equal response.status, 200
-    assert_includes response.body, "Custom Tab"
+    assert_includes response.body, "<li><a href=\"#custom_tab\" data-toggle=\"tab\">#{custom_tab_title}</a></li>"
   end
 end
