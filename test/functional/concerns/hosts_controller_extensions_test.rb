4dfda8c59c25a8cdc35866f6dcb253cc980486ff
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-10 19:27:22 -0500

    cleanup rubocop warnings in test

--- test/functional/concerns/hosts_controller_extensions_test.rb
+++ test/functional/concerns/hosts_controller_extensions_test.rb
@@ -1,12 +1,15 @@
 require 'test_plugin_helper'
 
-class HostsControllerTest < ActionController::TestCase 
-  let(:os){FactoryBot.create(:operatingsystem, name: 'CentOS', major: '7', type: 'Redhat')}
-  let(:arch){FactoryBot.create(:architecture)}
-  let(:host_id){'foreman.example.com'}
-  let(:host){FactoryBot.create(:host, id: host_id, mac: '00:00:00:00:00:00', ip: '127.0.0.1', operatingsystem: os, arch: arch)}         
-  let(:custom_tab_title){SETTINGS[:custom_tab][:title]} 
-  
+class HostsControllerTest < ActionController::TestCase
+  let(:os) { FactoryBot.create(:operatingsystem, name: 'CentOS', major: '7', type: 'Redhat') }
+  let(:arch) { FactoryBot.create(:architecture) }
+  let(:host_id) { 'foreman.example.com' }
+  # rubocop:disable Metrics/LineLength
+  let(:host) { FactoryBot.create(:host, id: host_id, mac: '00:00:00:00:00:00', ip: '127.0.0.1', operatingsystem: os, arch: arch) }
+  # rubocop:enable Metrics/LineLength
+  let(:custom_tab_title) { SETTINGS[:custom_tab][:title] }
+
+  # rubocop:disable Metrics/LineLength, Style/StringLiterals, HttpPositionalArguments
   test 'GET hosts/:id displays tabs' do
     get :show, { :id => host.id }, set_session_user
     assert_includes response.headers['Content-Type'], 'text/html'
@@ -35,7 +38,7 @@ class HostsControllerTest < ActionController::TestCase
   test "host/:id contains custom_tab ajax link" do
     get :show, { :id => host.id }, set_session_user
 
-    assert_equal response.status, 200    
+    assert_equal response.status, 200
     assert_includes response.headers['Content-Type'], 'text/html'
     assert_includes response.body, "<ul id=\"myTab\""
     assert_includes response.body, "<li><a href=\"#custom_tab\" data-toggle=\"tab\">#{custom_tab_title}</a></li>"
@@ -52,18 +55,20 @@ class HostsControllerTest < ActionController::TestCase
     after do
       Rails.application.reload_routes!
     end
-  
+
     test 'AJAX request hosts/:id/custom_tab displays custom host data' do
-      skip("xhr route missing: ActionController::UrlGenerationError: No route matches {:action=>'/hosts/host6/custom_tab', :controller=>'hosts'}")  
-      #get "/hosts/#{host.name}/custom_tab", xhr: true 
-      #xhr :get, custom_tab_host_path(host), delivery_method: :deliver, status: :pending, format: 'js'
-      get custom_tab_host_path(host), xhr: true 
-      #get "/hosts/#{host_id}/custom_tab"
-      
+      skip("xhr route missing: ActionController::UrlGenerationError:
+            No route matches {:action=>'/hosts/host6/custom_tab', :controller=>'hosts'}")
+      # get "/hosts/#{host.name}/custom_tab", xhr: true
+      # xhr :get, custom_tab_host_path(host), delivery_method: :deliver, status: :pending, format: 'js'
+      get custom_tab_host_path(host), params: { xhr: true }
+      # get "/hosts/#{host_id}/custom_tab"
+
       SETTINGS[:custom_tab][:fields].each do |title, attr|
         assert_includes response.body, "<td>#{title}</td>"
         assert_includes response.body, "<td>#{host.get_chained_attr_val(attr)}</td>"
       end
-    end      
+    end
   end
+  # rubocop:enable Metrics/LineLength, Style/StringLiterals, HttpPositionalArguments
 end
