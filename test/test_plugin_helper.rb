7557abd8b329adbd4b45cae98b6f6d399af56912
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-10 19:27:22 -0500

    cleanup rubocop warnings in test

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -2,10 +2,10 @@
 require 'test_helper'
 
 class Object
-  def get_chained_attr_val attrs
+  def get_chained_attr_val(attrs)
     attr_val = nil
     attrs.split('.').each_with_index do |method, index|
-      attr_val = index.eql?(0) ? self.try(method) : attr_val.try(method)
+      attr_val = index.eql?(0) ? try(method) : attr_val.try(method)
     end
     attr_val
   end
