e4295b467c4ffe372a00943a7aa99323d9e6d7e1
wimmeldj <wimmeldj@miamioh.edu>
2018-04-12 15:39:27 -0400

    Fixes issue where foreman custom tab was not showing

--- app/overrides/custom_add_tab.rb
+++ app/overrides/custom_add_tab.rb
@@ -1,6 +1,6 @@
 if SETTINGS[:custom_tab]
   Deface::Override.new(:virtual_path => 'hosts/show',
-                       :name => 'add_tab_link',
+                       :name => 'custom_add_tab',
                        :insert_bottom => 'ul#myTab',
                        :text =>
                        "<li><a href='#custom_tab' data-toggle='tab'><%= _(custom_tab_title) %></a></li>")
